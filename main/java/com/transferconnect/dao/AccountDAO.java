10deaaec648632ae3453b9401af715ee19fc10ca
ktahmd <149021786+ktahmd@users.noreply.github.com>
2025-03-04 08:05:52 +0000

    ajouter de methode getbytel et getbynni

--- main/java/com/transferconnect/dao/AccountDAO.java
+++ main/java/com/transferconnect/dao/AccountDAO.java
@@ -8,14 +8,46 @@ import java.util.Map;
 public class AccountDAO {
 	
 	private static Map<String, Account> Accounts = new HashMap<>();
-  
 	static {
-    	Accounts.put("80029485392",new Account("80029485392","9801370078","Bankily",12000)); 
-    	Accounts.put("80053692732",new Account("80053692732","9801370078","Sadad",3000)); 
-    	Accounts.put("90337388484",new Account("90337388484","9801370078","Bimbank",4000)); 
+    	Accounts.put("AC1",new Account("AC1","9801370078","26303030","Bankily",12000)); //accountId, nni,Tel,bank,montant
+    	Accounts.put("AC2",new Account("AC2","9801370078","26303030","Sadad",3000)); 
+    	Accounts.put("AC3",new Account("AC3","9801370078","26303030","Bimbank",4000));
+    	Accounts.put("AC4",new Account("AC4","1234567890","26202020","Bimbank",4000));
+    	
     	
     }
+	
 
+	// Get user by constraint key (e.g., nni)
+	public Map<String, Account> getAccountByConstraintKey(String nni) {
+	    if (nni == null) {
+	        throw new IllegalArgumentException("NNI cannot be null");
+	    }
+	    
+	    Map<String, Account> result = new HashMap<>();
+	    for (Map.Entry<String, Account> entry : Accounts.entrySet()) {
+	        if (entry.getValue().getNni().equals(nni)) {
+	            result.put(entry.getKey(), entry.getValue());
+	        }
+	    }
+	    
+	    return result.isEmpty() ? null : result; // Return null if no user found
+	}
+	public Map<String, Account> getAccountByConstraintKeytel(String tel) {
+	    if (tel == null) {
+	        throw new IllegalArgumentException("NNI cannot be null");
+	    }
+	    
+	    Map<String, Account> result = new HashMap<>();
+	    for (Map.Entry<String, Account> entry : Accounts.entrySet()) {
+	        if (entry.getValue().getTel().equals(tel)) {
+	            result.put(entry.getKey(), entry.getValue());
+	        }
+	    }
+	    
+	    return result.isEmpty() ? null : result; // Return null if no user found
+	}
+    
     // Récupérer un compte par son ID
     public Account getAccountById(String accountId) {
         return Accounts.get(accountId);
